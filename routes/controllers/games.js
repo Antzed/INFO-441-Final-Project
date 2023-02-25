cae96f70b530868b3a33f13ce3108e23f9cc09d7
antzed <anthoz3@uw.edu>
2023-02-25 13:46:08 -0800

    able to fetch game names

--- routes/controllers/games.js
+++ routes/controllers/games.js
@@ -1,9 +1,39 @@
 import express from 'express';
+import request from 'request';
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.send('TODO: repsond with game data');
-});
+import { RAWG_APIKEY, X_RAPIDAPI_KEY } from '../../credentials.js';
 
+// rapid api options with a url variable
+let options = {
+    method: 'GET',
+    url: 'https://rawg-video-games-database.p.rapidapi.com/games?key=' + RAWG_APIKEY,
+    headers: {
+      'X-RapidAPI-Key': X_RAPIDAPI_KEY,
+      'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com',
+      useQueryString: true
+    }
+};
+
+
+/* GET games listing. */
+router.get('/', function(req, res, next) {
+    options.url += '&search=' +  req.query.search.toLowerCase();
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        
+        // put the names of the games into an array
+        let games = [];
+        let data = JSON.parse(body);
+        data.results.forEach(game => {
+            games.push(game.name);
+        });
+        console.log(games);
+        res.send(games);
+        options.url = 'https://rawg-video-games-database.p.rapidapi.com/games?key=' + RAWG_APIKEY;
+    });
+
+});
+// &search=grand%20theft%20auto%20v
 
 export default router;
