0afbd04429fd019a7e2343cbcd92fe7b2247feab
unknown <sij1@uw.edu>
2023-02-25 22:14:26 -0800

    added vote get and post

--- routes/controllers/users.js
+++ routes/controllers/users.js
@@ -18,6 +18,58 @@ router.get('/', function(req, res, next) {
 });
 
 
+// POST user's game vote
+router.post('/vote', async (req, res) => {
+  try {
+    if (req.session.isAuthenticated) {
+      let user = await req.models.User.find({userName: req.session.account.username});
 
+      const newVote = new req.models.Vote({
+        categoryID: req.body.categoryId,
+        categoryName: req.body.categoryName,
+        user: user._id,
+        game: req.body.game,
+        date: Date.now(),
+      });
+
+      await newVote.save();
+
+    } else {
+      res.send('Error: You must be logged in to vote for a game');
+    }
+  } catch(error) {
+    console.log("Error getting url preview: ", error);
+    res.status(500).json({status: "error", "error": error});
+  }
+});
+
+// GET user's game votes
+router.get('/vote', async (req, res) => {
+  try {
+    if (req.session.isAuthenticated) {
+      let user = await req.models.User.find({userName: req.session.account.username});
+      
+      // Check if user is in the database, if not add
+      if (!user) {
+        const newUser = new req.models.User({
+          userName: req.session.account.username,
+        });
+
+        await newUser.save();
+        user = await req.models.User.find({userName: req.session.account.username});
+      }
+
+      let allVotes = await req.models.Vote.find({user: user._id});
+
+      // May need to map this, will do later
+      res.json(allVotes);
+    } else {
+      res.send('Error: You must be logged in to vote for a game');
+    }
+  } catch(error) {
+    console.log("Error getting url preview: ", error);
+    res.status(500).json({status: "error", "error": error});
+  }
+});
 
 export default router;
