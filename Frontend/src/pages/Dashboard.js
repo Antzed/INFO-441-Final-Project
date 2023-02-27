865860663d6765f27fd736b133ebce881f5dd316
William Wang <swang45@uw.edu>
2023-02-26 18:22:26 -0800

    background finished

--- Frontend/src/pages/Dashboard.js
+++ Frontend/src/pages/Dashboard.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { useEffect, useState} from 'react';
-import Background from '../components/Background';
-import Gamebox from '../components/Gamebox';
 
+import Gamebox from '../components/Gamebox';
+// 
 function Dashboard() {
     return (
-        <div className='h-[100vh] flex flex-col justify-center items-center' style={{"background": "linear-gradient(92.92deg, #1139CB -8.48%, #6B22A5 95.28%)"}}>
-            <h1 class='text-white'>Welcome to your Dashboard!</h1>
-            <div class='flex flex-col items-center'>
-                <div class='flex flex-row space-x-12'>
-                    <Gamebox/>
-                    <Gamebox/>
-                    <Gamebox/>
-                </div>
-                <div class='flex flex-row space-x-12'>
-                    <Gamebox/>
-                    <Gamebox/>
-                    <Gamebox/>
-                </div>
+      <div
+        className="h-[100vh] flex flex-col justify-center items-center"
+        style={{
+          background:
+            "linear-gradient(92.92deg, #1139CB -8.48%, #6B22A5 95.28%)",
+        }}>
+        <img
+          className="absolute max-w-[100%] left-[40%] lg:top-[-70%] z-[1]"
+          src="/background.png"
+          alt="asdf"
+        />
+        <div className='z-[2]'>
+          <h1 class="text-white">Welcome to your Dashboard!</h1>
+          <div class="flex flex-col items-center">
+            <div class="flex flex-row space-x-12">
+              <Gamebox />
+              <Gamebox />
+              <Gamebox />
             </div>
+            <div class="flex flex-row space-x-12">
+              <Gamebox />
+              <Gamebox />
+              <Gamebox />
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 export default Dashboard;
