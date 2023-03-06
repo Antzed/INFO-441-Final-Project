0483bc08ebe743321bcaff7dd2fe0099e8d4cdb2
unknown <sij1@uw.edu>
2023-03-05 18:59:27 -0800

    added try catch to all endpoints

--- routes/controllers/categories.js
+++ routes/controllers/categories.js
@@ -4,12 +4,17 @@ var router = express.Router();
 
 router.get('/', async function(req, res, next) {
     // get the category name from mongoDB
-    const categories = await req.models.Category.find({});
-    let categoryNames = [];
-    categories.forEach(category => {
-        categoryNames.push(category.name);
-    });
-    res.send(categoryNames);
+    try {
+        const categories = await req.models.Category.find({});
+        let categoryNames = [];
+        categories.forEach(category => {
+            categoryNames.push(category.name);
+        });
+        res.send(categoryNames);
+    } catch(error) {
+        console.log("Error getting category names: ", error);
+        res.status(500).json({status: "error", "error": error});
+    }
 });
 
 
--- routes/controllers/games.js
+++ routes/controllers/games.js
@@ -18,46 +18,59 @@ let options = {
 
 /* GET games listing. */
 router.get('/', function(req, res, next) {
-    options.url += '&search=' +  req.query.search.toLowerCase();
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        
-        // put the names of the games into an array
-        let games = [];
-        let data = JSON.parse(body);
-        data.results.forEach(game => {
-            games.push(game.name);
+    try {
+        options.url += '&search=' +  req.query.search.toLowerCase();
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            
+            // put the names of the games into an array
+            let games = [];
+            let data = JSON.parse(body);
+            data.results.forEach(game => {
+                games.push(game.name);
+            });
+            console.log(games);
+            res.send(games);
+            options.url = 'https://rawg-video-games-database.p.rapidapi.com/games?key=' + RAWG_APIKEY;
         });
-        console.log(games);
-        res.send(games);
-        options.url = 'https://rawg-video-games-database.p.rapidapi.com/games?key=' + RAWG_APIKEY;
-    });
-
+    } catch(error) {
+        console.log("Error getting list of games: ", error);
+        res.status(500).json({status: "error", "error": error});
+    }
 });
 
 router.get('/imgs', function(req, res, next) {
-    options.url += '&search=' +  req.query.search.toLowerCase();
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-
-        //put the image linkes of the games into an array
-        let imgs = [];
-        let data = JSON.parse(body);
-        data.results.forEach(game => {
-            if (game.background_image) {
-                imgs.push(game.background_image);
-            }
+    try {
+        options.url += '&search=' +  req.query.search.toLowerCase();
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+    
+            //put the image linkes of the games into an array
+            let imgs = [];
+            let data = JSON.parse(body);
+            data.results.forEach(game => {
+                if (game.background_image) {
+                    imgs.push(game.background_image);
+                }
+            });
+            console.log(imgs);
+            res.send(imgs);
+            options.url = 'https://rawg-video-games-database.p.rapidapi.com/games?key=' + RAWG_APIKEY;
         });
-        console.log(imgs);
-        res.send(imgs);
-        options.url = 'https://rawg-video-games-database.p.rapidapi.com/games?key=' + RAWG_APIKEY;
-    });
-
+    } catch(error) {
+        console.log("Error getting images: ", error);
+        res.status(500).json({status: "error", "error": error}); 
+    }
 });
 
 router.get('/category', async function(req, res, next) {
-    const category = await req.models.Category.find();
-    res.json(category);
+    try {
+        const category = await req.models.Category.find();
+        res.json(category);
+    } catch(error) {
+        console.log("Error getting cateogy IDs: ", error);
+        res.status(500).json({status: "error", "error": error}); 
+    }
 });
 
 export default router;
