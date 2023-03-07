8cba65218e24b100b5fe35ba0071336d457546e6
William Wang <swang45@uw.edu>
2023-03-07 15:29:44 -0800

    finished game page styling

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -34,43 +34,59 @@ function Navbar(props) {
         console.log(selectedGame)
     }
     return (
-        <nav className="test-white py-4 fixed top-0 w-full z-[10]">
-            <div className="container flex flex-wrap items-center justify-between mx-auto">
-                <Link to='/'>
-                    <div className='text-white px-8 text-xl' onClick={handleBackToHome}>GameDash.</div>
-                </Link>
-                <form
-                id="search-form-nav"
-                className="flex flex-row items-center justify-between bg-transparent rounded-3xl p-4 w-1/5 h-2 border-2 border-white ml-[50%]">
-                <input
-                    className="py-3 px-3 bg-transparent flex-1 outline-none"
-                    name="searchTextNav"
-                    placeholder="Search Game"
-                    onChange={(e) => handleSearch(e)}
-                />
-                <Link
-                    to={'/game/' + selectedGame}
-                    query={{game: selectedGame}}
-                    form="search-form-nav"
-                    className="text-white scale-125">
-                    <FontAwesomeIcon icon={faSearch} />
-                </Link>
-                </form>
-                <div>
-                    {props.loggedIn ? ( 
-                    <div className='flex'>
-                        <button type="button" onClick={handleLogout} className="focus:outline-none text-white bg-accent-orange hover:bg-amber-500 focus:ring-4 focus:ring-accent-orange font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Logout</button>
-                        <button type="button" onClick={handleToUserDashboard} className="focus:outline-none text-white bg-accent-orange hover:bg-amber-500 focus:ring-4 focus:ring-accent-orange font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Your dashboard</button>
-                    </div>
-                    ) : ( 
-                        <div>
-                        <button type="button" onClick={handleLogin} className="focus:outline-none text-white bg-accent-orange hover:bg-amber-500 focus:ring-4 focus:ring-accent-orange font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Login</button>
-                        </div>
-                        )}
-                
-                </div>
+      <nav className="test-white py-4 fixed top-0 w-full z-[10] backdrop-blur-md">
+        <div className="container flex flex-wrap items-center justify-between mx-auto">
+          <Link to="/">
+            <div className="text-white px-8 text-xl" onClick={handleBackToHome}>
+              GameDash.
             </div>
-        </nav>
-    )
+          </Link>
+          <form
+            id="search-form-nav"
+            className="flex flex-row items-center justify-between bg-transparent rounded-3xl p-4 w-1/5 h-2 border-2 border-white ml-[50%]">
+            <input
+              className="py-3 px-3 bg-transparent flex-1 outline-none "
+              name="searchTextNav"
+              placeholder="Search Game"
+              onChange={(e) => handleSearch(e)}
+            />
+            <Link
+              to={"/game/" + selectedGame}
+              query={{ game: selectedGame }}
+              form="search-form-nav"
+              className="text-white scale-125 ">
+              <FontAwesomeIcon icon={faSearch} />
+            </Link>
+          </form>
+          <div>
+            {props.loggedIn ? (
+              <div className="flex">
+                <button
+                  type="button"
+                  onClick={handleLogout}
+                  className="focus:outline-none text-white bg-accent-orange hover:bg-amber-500 focus:ring-4 focus:ring-accent-orange font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
+                  Logout
+                </button>
+                <button
+                  type="button"
+                  onClick={handleToUserDashboard}
+                  className="focus:outline-none text-white bg-accent-orange hover:bg-amber-500 focus:ring-4 focus:ring-accent-orange font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
+                  Your dashboard
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button
+                  type="button"
+                  onClick={handleLogin}
+                  className="focus:outline-none text-white bg-accent-orange hover:bg-amber-500 focus:ring-4 focus:ring-accent-orange font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
+                  Login
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </nav>
+    );
 }
 export default Navbar;
--- Frontend/src/pages/Game.js
+++ Frontend/src/pages/Game.js
@@ -29,14 +29,14 @@ function Game() {
     }, [])
     
     //TODO: 1. need to map platforms to platformbox and display them. 2. need to display votes for each category (votes is an object)
-    return(
+    return (
       <>
-      <div
-        className="h-screen w-screen relative flex flex-col justify-center items-center overflow-hidden"
-        style={{
-          background:
-            "linear-gradient(92.92deg, #1139CB -8.48%, #6B22A5 95.28%)",
-        }}>
+        <div
+          className="min-h-[100vh] w-screen relative pt-[80px] overflow-hidden"
+          style={{
+            background:
+              "linear-gradient(92.92deg, #1139CB -8.48%, #6B22A5 95.28%)",
+          }}>
           <img
             className="absolute max-w-[100%] left-[40%] lg:top-[-70%] z-[1]"
             src="/background.png"
@@ -44,36 +44,58 @@ function Game() {
           />
           <div className="z-[2] mt-26 justify-center overflow-auto">
             <div className="flex flex-col justify-center max-w-3/4">
-            <h1 className="text-white justify-center flex-grow m-4">{gameName}</h1>
-            
-            <div className="flex flex-col items-center">
-              <img src="https://media.rawg.io/media/games/b4e/b4e4c73d5aa4ec66bbf75375c4847a2b.jpg" alt="game" className="object-cover w-1/2 h-1/4" />
-              {/* <img src={publicGameInfo.background_image} alt="game" className="object-cover w-2/3 h-1/3" /> */}
-              <div className="flex flex-row w-full justify-center ">
+              <h1 className="text-white justify-center flex-grow m-4">
+                {gameName}
+              </h1>
+
+              <div className="flex flex-col items-center">
+                <img
+                  src="https://media.rawg.io/media/games/b4e/b4e4c73d5aa4ec66bbf75375c4847a2b.jpg"
+                  alt="game"
+                  className="object-cover max-h-[300px] rounded-xl drop-shadow-xl"
+                />
+                {/* <img src={publicGameInfo.background_image} alt="game" className="object-cover w-2/3 h-1/3" /> */}
+                <div className="flex flex-row w-full justify-center ">
                   {/* {publicGameInfo.platforms.map((platform) => {
                       <Platformbox platform={platform.name} />
                   })} */}
                   <Platformbox platform="XBOX" />
                   <Platformbox platform="XBOX" />
-              </div>
-                  
-                  <h2 className="text-white justify-start flex-grow">Release Date</h2>
-                  <p>November 18, 2011</p>
+                </div>
+
+                <div className="mt-8 flex justify-evenly gap-4 max-w-[800px]">
+                  <div className="flex-1">
+                    <h2 className="text-white justify-start flex-grow">
+                      Release Date
+                    </h2>
+                    <p className="mb-8">November 18, 2011</p>
+                    <h2 className="text-white justify-start flex-grow">
+                      Votes
+                    </h2>
+                    {/* {votes.entries().map(keyValue => {
+                        <p>{keyValue[0]}: {keyValue[1]}</p>})
+                    } */}
+                    <p>test: 1 vote</p>
+                  </div>
                   <p>{publicGameInfo.released}</p>
-                  <h2 className="text-white justify-start flex-grow max-w-3/4">Description</h2>
-                  <p>Minecraft is a sandbox game developed by Mojang Studios. The game was created by Markus "Notch" Persson in the Java programming language.</p>
+                  <div className="flex-1">
+                    <h2 className="text-white justify-start flex-grow max-w-3/4">
+                      Description
+                    </h2>
+                    <p>
+                      Minecraft is a sandbox game developed by Mojang Studios.
+                      The game was created by Markus "Notch" Persson in the Java
+                      programming language.
+                    </p>
+                  </div>
                   <p>{publicGameInfo.description}</p>
-                  <h2 className="text-white justify-start flex-grow">Votes</h2>
-                  {/* {votes.entries().map(keyValue => {
-                      <p>{keyValue[0]}: {keyValue[1]}</p>})
-                  } */}
-                  <p>test: 1 vote</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </>
-    )
+    );
 }
 
 export default Game;
